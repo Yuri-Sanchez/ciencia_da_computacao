38f8b0d351b4f8a2f419611a781dc5faaa3830ee
Yuri-Sanchez <yuri.tecnologia@outlook.com>
2025-06-05 23:34:16 -0300

    Adicionando funcionalidades

--- aula_8.c
+++ aula_8.c
@@ -17,20 +17,33 @@ int main(){
     scanf("%d%d", &l, &c);
 
     int matriz[l][c];
+    int soma = 0, qimpar, qpar;
+    int qtd = l * c;
+    qimpar = qpar = 0;
 
     for(int i = 0; i < l; i++){
         for(int j = 0; j < c; j++){
             printf("\nDigite o componente [%d][%d]: ", i+1, j+1);
             scanf("%d", &matriz[i][j]);
+            soma += matriz[i][j];
+            
+            if(matriz[i][j] % 2 == 0)
+                qpar++;
+            else   
+                qimpar++;     
         }
     }
 
+    float media = (float)soma/qtd; //É necessário adicionar o "float" na variável soma porque as variáveis "soma" e "qtd" estão declaradas como    inteiras, então quando é feita a divisão, a variável média entende que a divisão por dois inteiro é um número inteiro;
     
     for(int i = 0; i < l; i++){
         for(int j = 0; j < c; j++){
             printf("\t%d", matriz[i][j]);
         }
         printf("\n");
-    }    
+    }
+    printf("\nA soma dos elementos da matriz é %d", soma);
+    printf("\nDos elementos digitados %d são impares e %d são pares", qimpar, qpar);    
+    printf("\nA média dos elementos é %.2f", media);
     return 0;
 }
